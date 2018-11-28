9ab21020267386d47e981d59b9fece2439c54871
Saulo Daniel <saulodaniel2006@gmail.com>
2018-11-28 01:25:35 -0200

    ajustes de código e integração relatorio 1 com Financeiro e RH

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -23,33 +23,83 @@ export class DashboardComponent implements OnInit {
   urlRH = "https://sigerh.azurewebsites.net/api/";
   urlMV = "https://sigemv.azurewebsites.net/api/";
   urlFinanceiro = "http://trabalhosige.azurewebsites.net/api/";
-  urlProd = "?";
+  urlProd = "https://production-api.azurewebsites.net/api/";
 
 
   listaFuncionarios: any[];
+  listaVendas: any[];
   ngOnInit(): void {
     this.listaFuncionarios = [];
+    this.listaVendas = [];
+    this.listaVendas = this.getVendas();
     this.listaFuncionarios = this.getFuncionarios();
+    this.montarRelatorio1();
+  }
+
+  montarRelatorio1(){
     var funci = [];
     this.listaFuncionarios.forEach(F => {
       if (F.Cargo == "Vendedor" && funci.length < 2)
         funci.push(F);
     });
-
     var funciProntos = [];
     var listaDrilldown = [];
-    funci.forEach((fp,index) => {
+    funci.forEach((fp, index) => {
+      var vendas = this.listaVendas.filter(function (v) {
+        return v.idFuncionario == fp.Matricula
+      });
+      var totalQuantidadeVendidaOtimo = 0;
+      var totalQuantidadeVendidaBom = 0;
+      var totalQuantidadeVendidaMedio = 0;
+      var totalQuantidadeVendidaRuim = 0;
+
+      var preco = this.getValorProduto();
+      vendas.forEach(v => {
+        switch (v.qualidade) {
+          case 1:
+          case 2:
+            break;
+
+          case 3:
+          case 4:
+            totalQuantidadeVendidaRuim += parseFloat((v.quantidade * preco).toFixed(2));
+            break;
+
+          case 5:
+          case 6:
+            totalQuantidadeVendidaMedio += parseFloat((v.quantidade * preco).toFixed(2));
+            break;
+
+          case 7:
+          case 8:
+            totalQuantidadeVendidaBom += parseFloat((v.quantidade * preco).toFixed(2));
+            break;
+
+          case 9:
+          case 10:
+            totalQuantidadeVendidaOtimo += parseFloat((v.quantidade * preco).toFixed(2));
+            break;
+        }
+      });
+
       var fvm = new FuncionarioVendasViewModel();
       fvm.name = fp.Nome;
-      fvm.y = 4000000 - ((index + 1 )) * 70000;
+      fvm.y = parseFloat((totalQuantidadeVendidaBom +
+        totalQuantidadeVendidaMedio +
+        totalQuantidadeVendidaOtimo +
+        totalQuantidadeVendidaRuim).toFixed(2));
       var dd = new SerieDrilldownViewModel();
       dd.id = "total" + fvm.name.trim();
       fvm.drilldown = dd.id;
       dd.colorByPoint = true;
       dd.name = "Tipos Coque x Valor"
-      dd.data.push(CoqueValor.returnCoqueValor("Ótimo", 2500000 - ((index + 1 )) * 70000));
-      dd.data.push(CoqueValor.returnCoqueValor("Bom", 1000000 - ((index + 1 )) * 70000));
-      dd.data.push(CoqueValor.returnCoqueValor("Médio", 750000 - ((index + 1 )) * 70000));
+
+      if (totalQuantidadeVendidaOtimo > 0)
+        dd.data.push(CoqueValor.returnCoqueValor("Ótimo", totalQuantidadeVendidaOtimo));
+      if (totalQuantidadeVendidaBom > 0)
+        dd.data.push(CoqueValor.returnCoqueValor("Bom", totalQuantidadeVendidaBom));
+      if (totalQuantidadeVendidaMedio > 0)
+        dd.data.push(CoqueValor.returnCoqueValor("Médio", totalQuantidadeVendidaMedio));
       //dd.data.push(CoqueValor.returnCoqueValor("Ruim", 250000 - ((index + 1 )) * 70000));
       funciProntos.push(fvm);
       listaDrilldown.push(dd);
@@ -82,13 +132,12 @@ export class DashboardComponent implements OnInit {
         series: [{
           name: 'Funcionários',
           colorByPoint: true,
-          data: funciProntos         
+          data: funciProntos
         }],
         drilldown: {
           series: listaDrilldown
         }
       });
-
 
   }
 
@@ -109,6 +158,26 @@ export class DashboardComponent implements OnInit {
       result = JSON.parse(res);
     })
     return result;
+  }
+
+  getVendas(): any {
+    var result;
+    this.httpGet(this.urlMV + "Vendas", res => {
+      result = JSON.parse(res);
+    })
+    return result;
+  }
+
+  getValorProduto() {
+    var result;
+    this.httpGet(this.urlFinanceiro + "Produto", res => {
+      result = JSON.parse(res);
+    })
+    var prod = result.filter(function (p) {
+      //TODO pegar da api de produção o produto a ser vendido
+      return p.id == 2
+    });;
+    return prod[0].preco;
   }
 
 
